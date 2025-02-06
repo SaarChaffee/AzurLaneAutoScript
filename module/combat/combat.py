dbe87940cae561c0d6283c23690077f0af6237ff
霞飛 <saarchaffee@qq.com>
2025-02-07 00:07:29 +0800

    fix

--- module/combat/combat.py
+++ module/combat/combat.py
@@ -101,8 +101,8 @@ class Combat(Level, HPBalancer, Retirement, SubmarineCall, CombatAuto, CombatMan
             return PAUSE_Neon
         if PAUSE_Cyber.match_template_color(self.device.image, offset=(10, 10)):
             return PAUSE_Cyber
-        if PAUSE_HolyLight.match_template_color(self.device.image, offset=(10, 10)):
-            return PAUSE_HolyLight
+        if Pause_HolyLight.match_template_color(self.device.image, offset=(10, 10)):
+            return Pause_HolyLight
         return False
 
     def handle_combat_quit(self, offset=(20, 20), interval=3):
