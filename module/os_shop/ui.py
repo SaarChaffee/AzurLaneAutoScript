101d9421df089302b4009379e9646ce303ae55e6
霞飛 <saarchaffee@qq.com>
2025-02-07 02:20:38 +0800

    change parameters

--- module/os_shop/ui.py
+++ module/os_shop/ui.py
@@ -14,7 +14,7 @@ OS_SHOP_SCROLL = AdaptiveScroll(
     OS_SHOP_SCROLL_AREA.button,
     parameters={
         'height': 255 - 99,
-        'prominence': 40,
+        'threshold': 40,
     },
     name="OS_SHOP_SCROLL"
 )
